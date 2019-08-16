c9a0c3df17ac258a977348dcfc95b549278d2fee
Atabey27 <48602960+Atabey27@users.noreply.github.com>
2019-08-16 17:33:36 +0300

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 clear
-echo -e "\e[4;31m SpeedX Productions !!! \e[0m"
+echo -e "\e[4;31m Atabey27 Productions !!! \e[0m"
 echo -e "\e[1;34m Presents \e[0m"
 echo -e "\e[1;32m TBomb \e[0m"
 echo "Press Enter To Continue"
 read a1
-if [[ -s update.speedx ]];then
+if [[ -s update.Atabey27 ]];then
 echo "All Requirements Found...."
 else
 echo 'Installing Requirements....'
@@ -17,7 +17,7 @@ apt install python3-lzma
 apt install python3-pip
 pip3 install urllib3
 pip3 install requests
-echo This Script Was Made By SpeedX >update.speedx
+echo This Script Was Made By Atabey27 >update.Atabey27
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -29,11 +29,11 @@ clear
 echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
-echo -e "\e[4;34m This Bomber Was Created By SpeedX \e[0m"
+toilet -f mono12 -F border Atabey27
+echo -e "\e[4;34m This Bomber Was Created By Atabey27 \e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
+echo -e "\e[1;32m           Mail: ggAtabey2729@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: https://bit.do/Atabey27git \e[0m"
 echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
 echo " "
 echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
@@ -55,7 +55,7 @@ exit 0
 elif [ $ch -eq 2 ];then
 clear
 echo -e "\e[1;32m"
-echo 'Call Bomb By SpeedX'> call.xxx
+echo 'Call Bomb By Atabey27'> call.xxx
 python3 bomber.py call
 rm *.xxx >/dev/null 2>&1
 exit 0
@@ -63,13 +63,13 @@ elif [ $ch -eq 3 ];then
 clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/TheSpeedX/TBomb
+git clone https://github.com/TheAtabey27/TBomb
 if [[ -s TBomb/TBomb.sh ]];then
 cd TBomb
 cp -r -f * .. > temp
 cd ..
 rm -rf  TBomb >> temp
-rm update.speedx >> temp
+rm update.Atabey27 >> temp
 rm temp
 chmod +x TBomb.sh
 fi
@@ -82,7 +82,7 @@ exit
 elif [ $ch -eq 4 ];then
 clear
 echo -e "\e[1;32m"
-echo 'Protect List By SpeedX'> protect.xxx
+echo 'Protect List By Atabey27'> protect.xxx
 python3 bomber.py protect
 rm *.xxx >/dev/null 2>&1
 elif [ $ch -eq 5 ];then
@@ -90,7 +90,7 @@ clear
 echo -e "\e[1;33m"
 figlet TBomb
 echo -e "\e[1;34mCreated By \e[1;34m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border Atabey27
 echo  " "
 echo -e "\e[1;32m                   Features\e[1;34m"
 echo "  [+] Unlimited And Super-Fast Bombing"
@@ -100,8 +100,8 @@ echo "  [+] Protection List"
 echo "  [+] Automated Future Updates"
 echo "  [+] Easy To Use And Embed in Code"
 echo -e "\e[1;32m                   Contributors\e[1;33m"
-echo -e "\e[1;33m      [*]  SpeedX   \e[1;31m"
-echo "         [-] Mail At: ggspeedx29@gmail.com"
+echo -e "\e[1;33m      [*]  Atabey27   \e[1;31m"
+echo "         [-] Mail At: ggAtabey2729@gmail.com"
 echo -e "\e[1;33m      [*]  The Black Hacker Roxstar   \e[1;31m"
 echo "         [-] Ping At: http://wa.me/917600140353"
 echo -e "\e[1;33m      [*]  Rieltar   \e[1;31m"
@@ -117,8 +117,8 @@ echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
 echo  " "
 echo -e "\e[4;31m That's All !!!\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
+echo -e "\e[1;32m           Mail: ggAtabey2729@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: https://bit.do/Atabey27git \e[0m"
 echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
 echo "Press Enter To Go Home"
 read a3
@@ -128,10 +128,10 @@ clear
 echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border Atabey27
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
+echo -e "\e[1;32m           Mail: ggAtabey2729@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: https://bit.do/Atabey27git \e[0m"
 echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
 echo " "
 exit 0
